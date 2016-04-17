5b912e6f52103147ceafcc8e82da422ea94f733c
David Mabodo <david.mabodo@gmail.com>
2016-04-17 20:28:16 +0200

    bases

---
+++ 1_Mapa/main.js
@@ -0,0 +1,18 @@
+
+/*var lang = getParameterByName('lang');
+if(lang!=null && lang!=""){
+  //set the locale in which the messages will be translated
+  iJS.i18n.setlocale(lang) ;
+  //add domain where to find messages data
+  iJS.i18n.bindtextdomain(lang, "../locale", "po") ;
+  //Always do this after a `setlocale` or a `bindtextdomain` call.
+  iJS.i18n.try_load_lang() ; //will load and parse messages data from the setting catalog.
+}
+
+//d3.select('.title').text(iJS._("Las cifras de la enfermedad del Chagas"));
+
+*/
+
+
+console.log('test')
+
