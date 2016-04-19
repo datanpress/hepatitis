ad8d919c6296168a3bc24ebc254633617794769c
David Mabodo <david.mabodo@gmail.com>
2016-04-19 21:54:34 +0200

    done

--- 1_Mapa/main.js
+++ 1_Mapa/main.js
@@ -26,11 +26,42 @@ var basic = new Datamap({
 
       return {path: path, projection: projection};
   },
+  geographyConfig: {
+    borderWidth: 0.5,
+    highlightOnHover: false,
+    popupOnHover: true,
+    popupTemplate: function(geography, data) { //this function should just return a string
+      if (data!=null){
+        if(data.fillKey=='diferenciado'){
+          if(geography.properties.name=='United States of America'){
+            return '<div class="hoverinfo"><strong>Precio diferenciado:</strong> 84.000$</div>';  
+          }else if(geography.properties.name=='Spain'){
+            return '<div class="hoverinfo"><strong>Precio diferenciado:</strong>: 25.000$</div>';  
+          }else if(geography.properties.name=='France'){
+            return '<div class="hoverinfo"><strong>Precio diferenciado:</strong>: 61.00$</div>';  
+          }else if(geography.properties.name=='United Kingdom'){
+            return '<div class="hoverinfo"><strong>Precio diferenciado:</strong>: 54.000$</div>';  
+          }else if(geography.properties.name=='Egypt'){
+            return '<div class="hoverinfo"><strong>Precio diferenciado:</strong>: 900$</div>';  
+          }else if(geography.properties.name=='India'){
+            return '<div class="hoverinfo"><strong>Precio diferenciado:</strong>: 900$</div>';  
+          }else{
+            console.log(geography.properties.name)
+            return '<div class="hoverinfo"><strong>Precio diferenciado:</strong></div>';  
+          }
+        }else if(data.fillKey=='generica'){
+          return '<div class="hoverinfo"><strong>Competencia genérica:</strong> 840$</div>';
+        }else if(data.fillKey=='voluntaria'){
+          return '<div class="hoverinfo"><strong>Licencia voluntaria:</strong> 2.000$</div>';
+        }
+      }
+    },
+  },
   fills: {
     defaultFill: "#CCC",
     diferenciado: "#f59c00",
-    generica: "#af9959",
-    voluntaria: "#d7ae1c"
+    generica: "#d7ae1c",
+    voluntaria: "#af9959"
   },
   data: {
     USA: { fillKey: "diferenciado" },
@@ -39,68 +70,79 @@ var basic = new Datamap({
     GBR: { fillKey: "diferenciado" },
     EGY: { fillKey: "diferenciado" },
     IND: { fillKey: "diferenciado" },
-    BGD: { fillKey: "voluntaria" },
-    AFG: { fillKey: "generica" },
-    NPL: { fillKey: "generica" },
-    MMR: { fillKey: "generica" },
-    VNM: { fillKey: "generica" },
-    MDG: { fillKey: "generica" },
-    LSO: { fillKey: "generica" },
-    MOZ: { fillKey: "generica" },
-    AGO: { fillKey: "generica" },
-    ZMB: { fillKey: "generica" },
-    MWI: { fillKey: "generica" },
-    TZA: { fillKey: "generica" },
-    UGA: { fillKey: "generica" },
-    COD: { fillKey: "generica" },
-    SSD: { fillKey: "generica" },
-    SDN: { fillKey: "generica" },
-    TCD: { fillKey: "generica" },
-    CAF: { fillKey: "generica" },
-    NER: { fillKey: "generica" },
-    BEN: { fillKey: "generica" },
-    LBR: { fillKey: "generica" },
-    SLE: { fillKey: "generica" },
-    TGO: { fillKey: "generica" },
-    BFA: { fillKey: "generica" },
-    MLI: { fillKey: "generica" },
-    MRT: { fillKey: "generica" },
-    SEN: { fillKey: "generica" },
-    GNB: { fillKey: "generica" },
-    GIN: { fillKey: "generica" },
-    GMB: { fillKey: "generica" },
-    YEM: { fillKey: "generica" },
-    GNQ: { fillKey: "generica" }
+    BGD: { fillKey: "generica" },
+    AFG: { fillKey: "voluntaria" },
+    NPL: { fillKey: "voluntaria" },
+    MMR: { fillKey: "voluntaria" },
+    VNM: { fillKey: "voluntaria" },
+    MDG: { fillKey: "voluntaria" },
+    LSO: { fillKey: "voluntaria" },
+    MOZ: { fillKey: "voluntaria" },
+    AGO: { fillKey: "voluntaria" },
+    ZMB: { fillKey: "voluntaria" },
+    MWI: { fillKey: "voluntaria" },
+    TZA: { fillKey: "voluntaria" },
+    UGA: { fillKey: "voluntaria" },
+    COD: { fillKey: "voluntaria" },
+    SSD: { fillKey: "voluntaria" },
+    SDN: { fillKey: "voluntaria" },
+    TCD: { fillKey: "voluntaria" },
+    CAF: { fillKey: "voluntaria" },
+    NER: { fillKey: "voluntaria" },
+    BEN: { fillKey: "voluntaria" },
+    LBR: { fillKey: "voluntaria" },
+    SLE: { fillKey: "voluntaria" },
+    TGO: { fillKey: "voluntaria" },
+    BFA: { fillKey: "voluntaria" },
+    MLI: { fillKey: "voluntaria" },
+    MRT: { fillKey: "voluntaria" },
+    SEN: { fillKey: "voluntaria" },
+    GNB: { fillKey: "voluntaria" },
+    GIN: { fillKey: "voluntaria" },
+    GMB: { fillKey: "voluntaria" },
+    YEM: { fillKey: "voluntaria" },
+    BTN: { fillKey: "voluntaria" },
+    LAO: { fillKey: "voluntaria" },
+    KHM: { fillKey: "voluntaria" },
+    TLS: { fillKey: "voluntaria" },
+    GNQ: { fillKey: "voluntaria" }
   }
 });
 
 
 basic.bubbles([
   {
-    name: 'Not a bomb, but centered on Brazil',
+    name: 'Vanuatu',
     radius: 5,
     centered: 'VUT',
     country: 'USA',
     yeild: 0,
-    fillKey: 'generica',
+    fillKey: 'voluntaria',
     date: '1954-03-01'
+  },
+  {
+    name: 'Tuvalu',
+    radius: 5,
+    fillKey: 'voluntaria',
+    latitude: -7.1095,
+    longitude: 177.6493
   },
   {
     radius: 5,
     name: 'Kiribati',
-    fillKey: 'generica',
+    fillKey: 'voluntaria',
     latitude: -3.3704,
     longitude: 168.7340
   },{
     radius: 5,
     name: 'Solomon Islands',
-    fillKey: 'generica',
+    fillKey: 'voluntaria',
     latitude: -9.6457,
     longitude: 160.1562
   },{
     radius: 5,
     name: 'Haiti',
-    fillKey: 'generica',
+    fillKey: 'voluntaria',
     latitude: 18.9712,
     longitude: -72.2853
   }
